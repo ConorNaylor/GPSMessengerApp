b267c29666f62b2cc93694636b58b8a98c467b0a
ConorNaylor <conornaylor@gmail.com>
2017-10-24 11:48:27 +0100

    Message Hisotry fields

--- app/src/main/java/com/example/conornaylor/gps_chat_app/MessageHistoryActivity.java
+++ app/src/main/java/com/example/conornaylor/gps_chat_app/MessageHistoryActivity.java
@@ -2,12 +2,49 @@ package com.example.conornaylor.gps_chat_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MessageHistoryActivity extends AppCompatActivity {
+
+    private String message;
+    private double d1,d2;
+    TextView messageHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_history);
+
+        messageHistory = (TextView) findViewById(R.id.messageHistory);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference myRef = database.getReference("Messages");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                    message = child.getKey();
+
+                    messageHistory.append("\n"+ message);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println("Couldn't read database");
+            }
+        });
     }
 }
